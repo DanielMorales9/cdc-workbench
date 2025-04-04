2a75e75a2ab47e851c09896b5b508c966949f50d
Daniel Morales <dnlmrls9@gmail.com>
2025-04-04 17:02:02 +0200

    fixes

--- confluent-registry/entrypoint.sh
+++ confluent-registry/entrypoint.sh
@@ -3,6 +3,6 @@ set -e
 
 pushd "$CONFLUENT_HOME"
 
-bin/schema-registry-start etc/schema-registry/schema-registry.properties
+bin/schema-registry-start /etc/schema-registry/schema-registry.properties
 
 popd
